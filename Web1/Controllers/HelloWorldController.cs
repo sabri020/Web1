4effb32a3b8270117b7405eaf1ee614c6591462f
sabri020 <122880409+sabri020@users.noreply.github.com>
2023-01-31 19:35:41 +0100

    Ajoutez des fichiers projet.

---
+++ Web1/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ Web1/Data/Web1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Web1.Models;
+
+namespace Web1.Data
+{
+    public class Web1Context : DbContext
+    {
+        public Web1Context (DbContextOptions<Web1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Web1.Models.Movie> Movie { get; set; }
+    }
+}
